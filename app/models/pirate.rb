efc8b728dabd855ad17981d214454a1936094484
gawaxx <jules.blanc@unccub.com>
2019-10-28 21:35:45 +0000

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,18 @@
 class Pirate
+
+    attr_accessor :weight, :height
+    attr_reader :name 
+
+    @@all = []
+
+    def initialize(params)
+        @name = params[:name]
+        @weight = params[:weight]
+        @height = params[:height]
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
 end
